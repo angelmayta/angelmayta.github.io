9e12484b90f16ecb5b3584c2e0b489cbe877be8e
Angel Mayta <angelmayta22@hotmail.com>
2021-09-06 00:36:20 -0500

    boton extraer

---
+++ js/lab05.js
@@ -0,0 +1,9 @@
+function extraer(){
+    //obtener valores de los campos
+    let apNom = document.getElementById("nombre").value;
+    let fNac = document.getElementById("fecha").value;
+
+    console.log(apNom);
+    console.log(fNac);
+}
+
